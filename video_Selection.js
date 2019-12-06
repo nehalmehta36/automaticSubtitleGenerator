8534ef22e2f7ecd2c31a0f865e9094d0c6c155d6
nehalmehta36 <nehalmehta36@gmail.com>
2019-12-06 14:46:55 +0530

    this is a rough draft

---
+++ video_Selection.js
@@ -0,0 +1,29 @@
+function myFunction(){
+  var x = document.getElementById("myFile");
+  var txt = "";
+  if ('files' in x) {
+    if (x.files.length == 0) {
+      txt = "Select a video file.";
+    } else {
+      for (var i = 0; i < x.files.length; i++) {
+        txt += "<br><strong>" + (i+1) + ". file</strong><br>";
+        var file = x.files[i];
+        if ('name' in file) {
+          txt += "name: " + file.name + "<br>";
+        }
+        if ('size' in file) {
+          txt += "size: " + file.size + " bytes <br>";
+        }
+      }
+    }
+  } 
+  else {
+    if (x.value == "") {
+      txt += "Select a video file.";
+    } else {
+      txt += "The files property is not supported by your browser!";
+      txt  += "<br>The path of the selected file: " + x.value;
+    }
+  }
+  document.getElementById("demo").innerHTML = txt;
+}
